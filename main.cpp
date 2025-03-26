15bcadbcb915a83a6d4a2f567f33d85ded275688
loganjwright <loganjwright1616@gmail.com>
2025-03-26 13:48:59 -0700

    Added say function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std;
 
+void say(string s);
 int main ()
 {
     cout << "Hey?" << endl;
     cout << "Hello." << endl;
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
