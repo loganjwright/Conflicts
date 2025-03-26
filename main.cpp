ca95c9ae8463d11baab657bcb0e1f39995358e6a
loganjwright <loganjwright1616@gmail.com>
2025-03-26 20:42:42 +0000

    Added function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+
 using namespace std;
+
+int add(int a, int b);
 
 int main ()
 {
     cout << "Hello." << endl;
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
